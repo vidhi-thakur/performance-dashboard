d71bf0267640f15dc1f887b22be0bfbc78223f93
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2026-08-20 22:30:07 +0530

    added test cases for interaction for filter component

--- src/components/Filter.test.jsx
+++ src/components/Filter.test.jsx
@@ -1,6 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
 import Filter from "./Filter";
+import userEvent from "@testing-library/user-event";
 
 describe("Filter", () => {
   it("renders filter header correctly", () => {
@@ -20,4 +21,61 @@ describe("Filter", () => {
     expect(screen.getByRole("button", { name: /cancel/i })).toBeInTheDocument();
     expect(screen.getByRole("button", { name: /apply/i })).toBeInTheDocument();
   });
+
+  it("renders correct number of options in each dropdown", () => {
+    render(<Filter handleApply={vi.fn()} handleCancel={vi.fn()} />);
+
+    const countrySelect = screen.getByLabelText("Country");
+    const customerTierSelect = screen.getByLabelText("Customer Tier");
+    const statusSelect = screen.getByLabelText("Status");
+
+    expect(within(countrySelect).getAllByRole("option")).toHaveLength(9);
+    expect(within(customerTierSelect).getAllByRole("option")).toHaveLength(4);
+    expect(within(statusSelect).getAllByRole("option")).toHaveLength(4);
+  });
+
+  it("renders default placeholder option as selected initially", () => {
+    render(<Filter handleApply={vi.fn()} handleCancel={vi.fn()} />);
+
+    const countrySelect = screen.getByLabelText("Country");
+    const customerTierSelect = screen.getByLabelText("Customer Tier");
+    const statusSelect = screen.getByLabelText("Status");
+
+    expect(countrySelect).toHaveValue("");
+    expect(customerTierSelect).toHaveValue("");
+    expect(statusSelect).toHaveValue("");
+  });
+
+  it("updates the country display value upon selecting a country option", async () => {
+    const user = userEvent.setup();
+    render(<Filter handleApply={vi.fn()} handleCancel={vi.fn()} />);
+
+    const countrySelect = screen.getByLabelText("Country");
+
+    await user.selectOptions(countrySelect, "India");
+    expect(countrySelect).toHaveValue("India");
+  });
+
+  it("updates the customer tier display value upon selecting a customer tier", async () => {
+    const user = userEvent.setup();
+    render(<Filter handleApply={vi.fn()} handleCancel={vi.fn()} />);
+
+    const customerTierSelect = screen.getByLabelText("Customer Tier");
+
+    await user.selectOptions(customerTierSelect, "Free");
+    expect(customerTierSelect).toHaveValue("Free");
+  });
+
+  it("updates the status display value upon selecting a status option", async () => {
+    const user = userEvent.setup();
+    render(<Filter handleApply={vi.fn()} handleCancel={vi.fn()} />);
+    const statusSelect = screen.getByLabelText("Status");
+
+    await user.selectOptions(statusSelect, "Completed");
+    expect(statusSelect).toHaveValue("Completed");
+  });
+
+  it("calls handleCancel when Cancel button is clicked", () => {
+
+  })
 });
